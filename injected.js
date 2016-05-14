b677ea52229cb063aca2c40e5d79204b968e8572
Chayoung You <yousbe@gmail.com>
2016-05-15 03:57:54 +0900

    Handle media detail view

--- injected.js
+++ injected.js
@@ -1,4 +1,4 @@
-function makePreview(link) {
+function makeMediaPreview(link) {
   var preview = document.createElement("div");
   preview.className = "js-media media-preview position-rel";
 
@@ -17,10 +17,38 @@ function makePreview(link) {
   return preview;
 }
 
+function makeMediaDetail(link) {
+  var detail = document.createElement("div");
+  detail.className = "tweet-detail-media";
+
+  var detailPreview = document.createElement("div");
+  detailPreview.className = "js-media media-preview detail-preview";
+
+  var previewContainer = document.createElement("div");
+  previewContainer.className = "js-media-preview-container position-rel margin-vm";
+
+  var previewLink = document.createElement("a");
+  previewLink.className = "js-media-image-link block med-link media-item is-zoomable";
+  previewLink.href = link;
+  previewLink.setAttribute("rel", "mediaPreview");
+  previewLink.setAttribute("target", "_blank");
+
+  var previewImage = document.createElement("img");
+  previewImage.className = "media-img";
+  previewImage.src = link;
+  previewImage.alt = "Media preview";
+
+  previewLink.appendChild(previewImage);
+  previewContainer.appendChild(previewLink);
+  detailPreview.appendChild(previewContainer);
+  detail.appendChild(detailPreview);
+  return detail;
+}
+
 function expandLinks(node) {
   var puushRegex = /https?:\/\/puu\.sh\/(?:[\w_]+\/)*[\w_]+\.(?:gif|jpe?g|png)/;
 
-  var tweets = node.querySelectorAll(".tweet-text");
+  var tweets = node.querySelectorAll(".js-tweet.tweet .tweet-text");
   for (var i = 0; i < tweets.length; i++) {
     var tweet = tweets[i];
     var links = tweet.querySelectorAll("a:not(.expanded)");
@@ -31,8 +59,28 @@ function expandLinks(node) {
         expandedURL = expandedURL.replace(/^http:\/\//, "https://");
         link.setAttribute("data-full-url", expandedURL);
         link.className += " expanded";
-        var preview = makePreview(expandedURL);
+        var preview = makeMediaPreview(expandedURL);
         tweet.parentNode.insertBefore(preview, tweet.nextSibling);
+      }
+    }
+  }
+
+  var tweetDetails = node.querySelectorAll(".js-tweet.tweet-detail .tweet-text");
+  for (var i = 0; i < tweetDetails.length; i++) {
+    var tweetDetail = tweetDetails[i];
+    var links = tweetDetail.querySelectorAll("a:not(.expanded)");
+    for (var j = 0; j < links.length; j++) {
+      var link = links[j];
+      var expandedURL = link.getAttribute("data-full-url");
+      if (puushRegex.test(expandedURL)) {
+        expandedURL = expandedURL.replace(/^http:\/\//, "https://");
+        link.setAttribute("data-full-url", expandedURL);
+        link.className += " expanded";
+        var detail = makeMediaDetail(expandedURL);
+        tweetDetail.parentNode.insertBefore(
+          detail,
+          tweetDetail.parentNode.querySelector(".js-tweet-media.tweet-detail-media")
+        );
       }
     }
   }
