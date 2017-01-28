5b852904e8e827340da835cf500a26a8aa21e185
Chayoung You <yousbe@gmail.com>
2017-01-29 04:37:24 +0900

    Remove debugging code

--- src/background.js
+++ src/background.js
@@ -1,18 +1,6 @@
 chrome.runtime.onMessage.addListener(onRuntimeMessage);
 
-function log() {
-  var args = [(new Date()).toLocaleString() + " |"];
-  for (var i = 0; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-
-  console.log.apply(console, args);
-};
-
 function onRuntimeMessage(msg, sender, sendResponse) {
-  if (chrome.runtime.lastError) {
-    log(chrome.runtime.lastError);
-  }
   var key = msg.type;
   switch (key) {
     case "pixiv":
@@ -35,9 +23,8 @@ function onRuntimeMessage(msg, sender, sendResponse) {
       document.querySelector("body").appendChild(img);
       break;
     default:
-      log("Unknown message " + key);
       sendResponse({error: true});
       break;
   }
   return true;
-};
+}
